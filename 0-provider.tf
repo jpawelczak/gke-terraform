e928c55d8e309d164bb626cf7e267e91e3306fbe
Jakub Pawelczak <jakub.pawelczak.pl@gmail.com>
2025-07-12 14:56:00 +0200

    initiated terraform with provider

---
+++ 0-provider.tf
@@ -0,0 +1,23 @@
+# 0-provider means it is the first part of the set up to start
+
+# terraform file with configurstion of provide
+# terraform plugin
+
+terraform {
+
+  required_version = ">= 1.0"
+
+  required_providers {
+
+# got it from Registry: https://registry.terraform.io/providers/hashicorp/aws/latest 
+    google = {
+      source = "hashicorp/google"
+      version = "6.43.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  region = "us-east-1"
+}
